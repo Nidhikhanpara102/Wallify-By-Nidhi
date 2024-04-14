985229412afafdc247f4e351b006bf65ef6da695
Nidhikhanpara102 <nidhikhanpara102@gmail.com>
2024-04-14 17:44:23 -0400

    Added logic for splash screen and updated models.
    - Implemented logic for the splash screen to enhance user experience during application loading.
    - Updated data or software models to ensure compatibility with the latest requirements or improvements in functionality.

--- app/src/main/java/com/android/nidhiandroidapp/Models/APIResponse.java
+++ app/src/main/java/com/android/nidhiandroidapp/Models/APIResponse.java
@@ -4,23 +4,12 @@ import java.util.ArrayList;
 
 public class APIResponse {
     public int page;
-    public int per_page;
     public ArrayList<Photo> photos;
-    public String next_page;
-
     public int getPage() {
         return page;
     }
-
-    public int getPer_page() {
-        return per_page;
-    }
-
     public ArrayList<Photo> getPhotos() {
         return photos;
     }
 
-    public String getNext_page() {
-        return next_page;
-    }
 }
--- app/src/main/java/com/android/nidhiandroidapp/SplashScreenActivity.java
+++ app/src/main/java/com/android/nidhiandroidapp/SplashScreenActivity.java
@@ -1,19 +1,29 @@
 package com.android.nidhiandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
-}
+    }
 
 
 
