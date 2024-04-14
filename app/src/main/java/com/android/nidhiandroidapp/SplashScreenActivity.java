30e9b9a541406933209994f6a07de9ae9ac27ce9
Nidhikhanpara102 <nidhikhanpara102@gmail.com>
2024-04-14 17:28:14 -0400

    Layout for the splash screen created and also added java file for it.
    Layout for the splash screen created and also added SplashScreenActivity.java file.

---
+++ app/src/main/java/com/android/nidhiandroidapp/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.android.nidhiandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+}
+
+
+
